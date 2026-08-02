fd297368b7ce472315863f6af754baea768f4f2d
jayprakashm578 <jayprakashm578@gmail.com>
2026-08-02 21:02:04 +0530

    feat: complete day 4 DOM interaction

--- src/main.js
+++ src/main.js
@@ -16,7 +16,36 @@ document.querySelector("#app").innerHTML = `
 
       <p>This is my engineering operating system</p>
 
-      <button>Start Mission</button>
+      <button id="startBtn">Start Mission</button>
     </main>
     `
+
+const button=document.querySelector("#startBtn");
+console.log(button)
+
+function greet() {
+  console.log("Welcome");
+}
+
+greet();
+
+function startMission() {
+  document.querySelector("h1").textContent = "Mission Started Succesfully";
+  document.querySelector("p").textContent = "We are on a mission to explore the universe ";
+  document.querySelector("h2").textContent = "Mission Started";  
+  document.querySelector(".date").style.display="block";
+  document.querySelector("button").disabled=true;
+  document.querySelector("#startBtn").textContent="Mission Active";
+}
+// console.log(parseInt(Date.()));
+
+
+
+button.addEventListener("click", startMission);
+
+const dateSpan=document.getElementById("date-display");
+const today=new Date();
+
+const formattedDate=today.toLocaleDateString();
+dateSpan.textContent=formattedDate;
 
