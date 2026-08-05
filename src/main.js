d9240fd86b2c178f20725ade6dd297c68484bc79
jayprakashm578 <jayprakashm578@gmail.com>
2026-08-05 21:27:00 +0530

    feat: complete day 5 array methods

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ const user= {
 
 const missionCompleted=false;
 
-const skills=["HTML","CSS","JavaScript","Node.js"];
 
 
 document.querySelector("#app").innerHTML = `
@@ -28,14 +27,97 @@ document.querySelector("#app").innerHTML = `
       <h2>Welcome ${user.name}</h2>
       <p>Goal: ${user.goal}</p>
       <p>Country: ${user.country}</p>
-      <h2>Skills: ${skills.join(", ")}</h2>
+  
+         <ul id='my-skills'> </ul>
+         <ul id='my-technologies'> </ul>
       <h2 class="date" style="display:none">Date: <span id="date-display"></span></h2>
 
       <p>This is my engineering operating system</p>
+      <ul id="my-missions"></ul>
       <p class="status"></p>
       <button id="startBtn">Start Mission</button>
     </main>
     `
+
+//Skills    
+const skills=["HTML","CSS","JavaScript","Node.js"];
+
+function renderSkills() {
+
+  // let skillList="";
+  
+  // for (let skill of skills) {
+  //   skillList+=`<li>${skill}</li>`;
+  // }
+
+  
+  //Using map function to render skills
+  const skillList=skills.map((skill)=>`<li>${skill}</li>`).join("");
+  document.getElementById("my-skills").innerHTML ="Skills:"+ skillList;
+  
+}
+
+renderSkills();
+
+skills.push("Git");
+
+renderSkills();
+
+for(let i=0;i<skills.length;i++){
+  if(skills[i]==="JavaScript"){
+    console.log("Favourite skill "+`${skills[i]}`)
+  }
+ else {
+    console.log(`Learning ${skills[i]}`);
+}
+}
+
+//Missions
+const missions = [
+  "Learn Javascript",
+  "Master Node.js",
+  "Learn Express",
+  "Learn MongoDB"
+]
+
+function renderMissions() {
+  let missionList=""
+ let a=1;
+  for (let mission of missions) {
+    missionList+=`<li>Mission ${a}  - ${mission}</li>`;
+    a+=1;
+  }
+
+  document.getElementById("my-missions").innerHTML=missionList;
+
+}
+
+renderMissions();
+
+//Technologies
+const technologies = ["HTML", "CSS", "JavaScript", "Node.js", "Express", "MongoDB"];
+
+const technologyArray=technologies.map((tech)=>` <li>${tech}</li>`);
+
+document.getElementById("my-technologies").innerHTML="Technologies: "+technologyArray.join("");
+
+let upperCase=technologies.map((tech)=>tech.toUpperCase())
+console.log("UpperCase Technologies: "+upperCase);
+
+let techLength=technologies.filter((tech)=>tech.length>5);
+console.log("Technologies with Length greater than 5: "+techLength);
+
+let searchTech=technologies.find((tech)=>tech==="Node.js");
+console.log("Found: "+searchTech);
+
+if(technologies.includes("React")){
+  console.log("React installed.");
+}
+else{
+  console.log("React not installed.");
+}
+
+technologies.forEach((tech)=>console.log("Learning "+tech));
 
 const button=document.querySelector("#startBtn");
 console.log(button)
@@ -72,3 +154,14 @@ if(missionCompleted) {
 else{
     document.querySelector(".status").textContent="Mission in Progress";
 }
+
+
+  for(let i=1;i<=5;i++){
+    if(i===1){
+      console.log("Mission Started...")
+    }
+    console.log(i);
+    if(i===5){
+      console.log("Mission Complete")
+    }
+  }
