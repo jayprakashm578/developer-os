98f461ff180faa9816724ae25730aa1a350afe01
jayprakashm578 <jayprakashm578@gmail.com>
2026-08-07 00:27:50 +0530

    feat: complete day 8 function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 import {add, multiply} from './math.js';
 
-console.log(add(12, 8));
-console.log(multiply(5, 7));
+// console.log(add(12, 8));
+// console.log(multiply(5, 7));
--- src/main.js
+++ src/main.js
@@ -15,7 +15,33 @@ const user= {
 
 const missionCompleted=false;
 
+// function greetUser(name)
+// {
+//   return `Welcome ${name}`;
+// }
 
+const greetUser=(name)=>{
+  return `Welcome ${name}`;
+}
+
+const multiply=function(a,b){
+  return a*b;
+}
+console.log(multiply(5,8));
+
+
+
+//Missions
+const missions = [
+  "Learn Javascript",
+  "Master Node.js",
+  "Learn Express",
+  "Learn MongoDB"
+]
+
+function totalMissions(array) {
+  return array.length;
+}
 
 document.querySelector("#app").innerHTML = `
     <header>
@@ -24,7 +50,7 @@ document.querySelector("#app").innerHTML = `
     </header>
 
     <main>
-      <h2>Welcome ${user.name}</h2>
+      <h2>${greetUser(user.name)}</h2>
       <p>Goal: ${user.goal}</p>
       <p>Country: ${user.country}</p>
   
@@ -33,6 +59,7 @@ document.querySelector("#app").innerHTML = `
       <h2 class="date" style="display:none">Date: <span id="date-display"></span></h2>
 
       <p>This is my engineering operating system</p>
+      <p>My Total Missions: ${totalMissions(missions)}</p>
       <ul id="my-missions"></ul>
       <p class="status"></p>
       <button id="startBtn">Start Mission</button>
@@ -65,20 +92,20 @@ renderSkills();
 
 for(let i=0;i<skills.length;i++){
   if(skills[i]==="JavaScript"){
-    console.log("Favourite skill "+`${skills[i]}`)
+    // console.log("Favourite skill "+`${skills[i]}`)
   }
  else {
-    console.log(`Learning ${skills[i]}`);
+    // console.log(`Learning ${skills[i]}`);
 }
 }
 
-//Missions
-const missions = [
-  "Learn Javascript",
-  "Master Node.js",
-  "Learn Express",
-  "Learn MongoDB"
-]
+function hasSkill(skill) {
+  return skills.includes(skill);
+}
+
+console.log(hasSkill("Node.js"));
+console.log(hasSkill("Python"));
+
 
 function renderMissions() {
   let missionList=""
@@ -94,42 +121,58 @@ function renderMissions() {
 
 renderMissions();
 
+
+function executeMission(callback) {
+  console.log("Mission Started");
+  callback();
+  console.log("Mission Completed");
+}
+
+executeMission(function(){
+  console.log("Learning JavaScript");
+})
+
 //Technologies
 const technologies = ["HTML", "CSS", "JavaScript", "Node.js", "Express", "MongoDB"];
 
-const technologyArray=technologies.map((tech)=>` <li>${tech}</li>`);
+// const technologyArray=technologies.map((tech)=>` <li>${tech}</li>`);
+// document.getElementById("my-technologies").innerHTML="Technologies: "+technologyArray.join("");
 
-document.getElementById("my-technologies").innerHTML="Technologies: "+technologyArray.join("");
+document.getElementById("my-technologies").innerHTML="Technologies: " + technologies.map((tech)=>` <li>${tech}</li>`).join("");
+
 
 let upperCase=technologies.map((tech)=>tech.toUpperCase())
-console.log("UpperCase Technologies: "+upperCase);
+// console.log("UpperCase Technologies: "+upperCase);
 
 let techLength=technologies.filter((tech)=>tech.length>5);
-console.log("Technologies with Length greater than 5: "+techLength);
+// console.log("Technologies with Length greater than 5: "+techLength);
 
 let searchTech=technologies.find((tech)=>tech==="Node.js");
-console.log("Found: "+searchTech);
+// console.log("Found: "+searchTech);
 
 if(technologies.includes("React")){
-  console.log("React installed.");
+  // console.log("React installed.");
 }
 else{
-  console.log("React not installed.");
+  // console.log("React not installed.");
 }
 
-technologies.forEach((tech)=>console.log("Learning "+tech));
+// technologies.forEach((tech)=>console.log("Learning "+tech));
 
 const button=document.querySelector("#startBtn");
-console.log(button)
+// console.log(button)
 
-function greet() {
+// function greet() {
+//   console.log("Welcome");
+// }
+
+const greet=()=>{
   console.log("Welcome");
-}
-
+} 
 greet();
 
 function startMission() {
-  document.querySelector("h1").textContent = "Mission Started Succesfully";
+  document.querySelector("h1").textContent = "Mission Started Successfully";
   document.querySelector(".subtitle").textContent = "We are on a mission to explore the universe ";
   document.querySelector("h2").textContent = "Mission Started";  
   document.querySelector(".date").style.display="block";
@@ -158,10 +201,10 @@ else{
 
   for(let i=1;i<=5;i++){
     if(i===1){
-      console.log("Mission Started...")
+      // console.log("Mission Started...")
     }
-    console.log(i);
+    // console.log(i);
     if(i===5){
-      console.log("Mission Complete")
+      // console.log("Mission Complete")
     }
   }
